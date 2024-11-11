6a7250ba1d89653f829c87d2feb705fa40de95b0
Alonso256 <alonsocl2001@gmail.com>
2024-11-11 16:43:16 +0100

    ejemplo sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Mybutton from './Mybutton'
+import Layout from './app/Layout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Layout>
+      <h1>My App</h1>
+      <p>Some content</p>
+    </Layout>
       <div>
         <Mybutton />
         <a href="https://vite.dev" target="_blank">
